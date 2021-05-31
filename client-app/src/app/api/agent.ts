d3d5de7f8273f88bd7e707ed4344e29cec23a611
robotdostopif <aron.cederlund@outlook.com>
2021-05-31 10:00:39 +0200

    Changes to errorhandling

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,8 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { toast } from 'react-toastify';
+import { history } from '../../index';
 import { Activity } from '../models/activity';
+import { store } from '../stores/store';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -10,13 +13,40 @@ const sleep = (delay: number) => {
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
 axios.interceptors.response.use(async response => {
-    try {
         await sleep(1000);
         return response;
-    } catch (error) {
-        console.log(error);
-        return await Promise.reject(error);
+}, (error: AxiosError) => {
+    const {data, status, config} = error.response!;
+    switch (status) {
+        case 400:
+            if (typeof data === 'string') {
+                toast.error(data);
+            }
+            if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
+                history.push('/not-found');
+            }
+            if(data.error) {
+                const modalStateErrors = [];
+                for(const key in data.errors) {
+                    if(data.errors[key]) {
+                        modalStateErrors.push(data.errors[key]);
+                    }
+                }
+                throw modalStateErrors.flat();
+            }
+            break;
+        case 401:
+            toast.error('unauthorized');
+            break;
+        case 404:
+            history.push('/not-found');
+            break;
+        case 500:
+            store.commonStore.setServerError(data);
+            history.push('/server-error');
+            break;
     }
+    return Promise.reject(error);
 })
 
 const responseBody = <T> (response: AxiosResponse<T>) => response.data;
@@ -30,10 +60,10 @@ const requests = {
 
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
-    details: (id: string) => requests.get<Activity>(`activities/${id}`),
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: Activity) => axios.post<void>('/activities', activity),
-    update: (activity: Activity) => axios.put<void>(`activities/${activity.id}`, activity),
-    delete: (id: string) => axios.delete<void>(`activities/${id}`)
+    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
 const agent = {
