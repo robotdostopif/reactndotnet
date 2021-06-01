8c4aabae92e36292d96ed8b6ecfcda5a1c674658
robotdostopif <aron.cederlund@outlook.com>
2021-06-01 13:27:17 +0200

    Formik implementation

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import 'react-calendar/dist/Calendar.css';
+import 'react-datepicker/dist/react-datepicker.css'
 import 'react-toastify/dist/ReactToastify.min.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
