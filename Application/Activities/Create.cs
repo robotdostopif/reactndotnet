245fa908fc3d93c3e3c5882077d20b75f3587978
robotdostopif <aron.cederlund@outlook.com>
2021-06-07 14:33:56 +0200

    Added attendees

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,23 +1,25 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
 {
     public class Create
     {
-        public class Command : IRequest<Result<Unit>> 
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
 
         public class CommandValidator : AbstractValidator<Command>
         {
-            public CommandValidator() 
+            public CommandValidator()
             {
                 RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
             }
@@ -26,18 +28,31 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private DataContext _context;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
             }
 
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                var user = await _context.Users.FirstOrDefaultAsync(x => 
+                x.UserName == _userAccessor.GetUsername());
+                var attendee = new ActivityAttendee
+                {
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true
+                };
+
+                request.Activity.Attendees.Add(attendee);
+
                 _context.Activities.Add(request.Activity);
 
                 var result = await _context.SaveChangesAsync() > 0;
-                if(!result) return Result<Unit>.Failure("Failed to create activity");
+                if (!result) return Result<Unit>.Failure("Failed to create activity");
                 return Result<Unit>.Success(Unit.Value);
             }
         }
